ca58baab8a2020431dc0cc29f9a3ac90ac476f69
DanilloCorvalan <danillo.corvalan@gmail.com>
2013-07-28 01:33:57 -0300

    server notification events

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function(socket) {
-  socket.on('idea-commented', function(data) {
-    io.sockets.emit('idea-inserted', data);
-  });
+	broadcastEventBuilder(socket)
+		.addEvent('idea-new-comment')
+		.addEvent('idea-comment-removed')
+		.addEvent('idea-rate-like')
+		.addEvent('idea-rate-dislike');
 });
+
+function broadcastEventBuilder(theSocket) {
+	var socket = theSocket;
+
+	var addEvent = function(eventName) {
+		socket.on(eventName, function(data) {
+			socket.broadcast.emit(eventName, data);
+		});
+		return this;
+	}
+
+	return {
+		addEvent : addEvent
+	};
+}
